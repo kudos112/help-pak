a1a7d6c0a1bd6b2f0e380fa034dae1cce450f4f0
Abdul Quddoos <53150433+abdul-quddous@users.noreply.github.com>
2022-05-16 00:48:26 +0500

    removed issues

--- pages/testing.js
+++ pages/testing.js
@@ -1,16 +1,15 @@
 import {Flex} from "@chakra-ui/react";
-import ReactZmage from "~/components/data/react-zmage/react-zmage.component";
 
 const Testing = () => {
-  const images = [
-    "https://zmage.caldis.me/imgSet/childsDream/5.jpg",
-    "https://zmage.caldis.me/imgSet/childsDream/6.jpg",
-  ];
+  // const images = [
+  //   "https://zmage.caldis.me/imgSet/childsDream/5.jpg",
+  //   "https://zmage.caldis.me/imgSet/childsDream/6.jpg",
+  // ];
   //   return <div>hel</div>;
   return (
     <Flex>
-      {" "}
-      <ReactZmage images={images} />{" "}
+      {"Testing"}
+      {/* <ReactZmage images={images} />{" "} */}
     </Flex>
   );
 };
